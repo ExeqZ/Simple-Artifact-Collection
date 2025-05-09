ba4be934b446b2c522e0021d8c10c0aadef1db32
exeqz <florian@amport.me>
2025-05-09 14:13:58 +0200

    Test1

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient
+
 import os
 import uuid
 
@@ -11,17 +10,17 @@ STORAGE_ACCOUNT_URL = os.environ["STORAGE_ACCOUNT_URL"]
 CONTAINER_NAME = "uploads"
 
 # Authenticate with Managed Identity
-credential = DefaultAzureCredential()
-blob_service_client = BlobServiceClient(account_url=STORAGE_ACCOUNT_URL, credential=credential)
+#credential = DefaultAzureCredential()
+#blob_service_client = BlobServiceClient(account_url=STORAGE_ACCOUNT_URL, credential=credential)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=str(uuid.uuid4()) + "_" + file.filename)
-            blob_client.upload_blob(file.read())
-            return "Upload successful!"
+#    if request.method == 'POST':
+ #       file = request.files['file']
+ #       if file:
+  #          blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=str(uuid.uuid4()) + "_" + file.filename)
+   #         blob_client.upload_blob(file.read())
+    #        return "Upload successful!"
     return render_template('index.html')
 
 if __name__ == '__main__':
